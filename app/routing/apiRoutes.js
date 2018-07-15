f6cf2735025474dd47f42e6dc6a71ac3ac034194
Christine Bonk <christinebonk@gmail.com>
2018-07-15 09:25:12 -0400

    writing new person to file

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,26 +1,9 @@
 var path = require("path");
 var fs = require("fs");
 var bodyParser = require('body-parser');
-var friends = require("../data/friends.js");
+var friends = JSON.parse(fs.readFileSync("app/data/friends.js", "utf8"));
 var MakePerson = require("../../makeperson.js")
 var topFriend = require("../../comparefriend.js")
-var you = {
-  "name":"Ahmed",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  "scores":[
-      5,
-      2,
-      4,
-      1,
-      5,
-      1,
-      2,
-      5,
-      4,
-      1
-    ]
-}
-
 
 function apiRoutes(app) {
 
@@ -36,13 +19,21 @@ function apiRoutes(app) {
 		var array = [request.body.q1, request.body.q2, request.body.q3, request.body.q4, request.body.q5, request.body.q6, request.body.q7, request.body.q8, request.body.q9, request.body.q10];
 
 		// contruct friend
-		// var you = new MakePerson(name,picture,array);
+		var you = new MakePerson(name,picture,array);
 		
 		//compare friends
 		topFriend(you, friends);
-		
+
+		//add you
+		friends.push(you);
+
+		friends = JSON.stringify(friends);
+		fs.writeFile(__dirname + "/../data/friends.js", friends, function (err) {
+		  if (err) return console.log(err);
+		});	
 	});
 }
+
 
 
 module.exports = apiRoutes;
